cc4b17072a7d65340010cf860ce00c8a8abd5a8a
Susan Winters <susanwinters214@gmail.com>
2023-01-11 16:32:16 -0600

    better defined the table row

--- app.js
+++ app.js
@@ -7,44 +7,58 @@ class Book {
     }
 }
 
-class Library { 
-   constructor( bookCount, books) {
+class Library {
+  constructor(bookCount, books) {
     this.bookCount = 0;
     this.books = [];
   }
 
-markRead(id) {
-    for (let i = 0;  i < this.bookCount.length;  i++)
-    {
-         if (this.books[i].id === id) {
-             this.books[i].isRead = true;
+  markRead(id) {
+    for (let i = 0; i < this.bookCount.length; i++) {
+      if (this.books[i].id === id) {
+        this.books[i].isRead = true;
              
-             // CHANGE HTML ckbox chked attr to TRUE
-           isRead.checked = checked;
+        // CHANGE HTML ckbox chked attr to TRUE
+        isRead.checked = checked;
            
-             break;
-            }
-    }    
+        break;
+      }
+    }
   }
 
- addBook(id, txtTitle, txtAuthor, isRead) {
-   this.books.push(new Book(id, txtTitle, txtAuthor, false)); 
+  addBook(txtTitle, txtAuthor, isRead) {
+    this.books.push(new Book(txtTitle, txtAuthor, isRead));
    
-   //Create new HTML table row with Title, Author & Read properties
-     row = `<tr><td>{txtTitle}</td><td>{txtAuthor}</td><td>{false}</td></tr>`;
-   
-     //Append the Row to the HTML table
-   let table = document.getElementById("table");
-   let row = table.insertRow(-1);
+    let tr = document.createElement("tr");
+    let titleTD = document.createElement("td");
+    titleTD.textContent = txtTitle;
+    let authorTD = document.createElement("td");
+    authorTD.textContent = txtAuthor;
+    let isReadTD = document.createElement("td");
+    isReadTD.type = "checkbox";
+
+    let checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.checked = isRead;
+
+    isReadTD.append(checkBox);
+    tr.append(titleTD);
+    tr.append(authorTD);
+    tr.append(isReadTD);
+
+    let table = document.getElementById("table");
+
+    table.append(tr);
+
    
     //Increment Library BookCount +1  
-     this.bookCount++;
+    this.bookCount++;
   }
 
-    removeBook(id) {
-       console.log("I will remove a specific book from the library");
-    //Select the table row with Title, Author & Read properties
-    //Remove the Row from the table
-    //Decrement Library BookCount -1
-  }
+
 }
+
+let library = new Library(0, []);
+function addBook(txtTitle, txtAuthor, isRead){
+  library.addBook(txtTitle, txtAuthor, isRead);
+};
